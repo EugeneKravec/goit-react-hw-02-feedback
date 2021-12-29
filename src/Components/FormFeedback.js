8fb8c11f5383ee5285b0c826c2e1c23daf786527
Eugene Kravec <krava255@gmail.com>
2021-12-30 00:54:10 +0200

    made function with state

--- src/Components/FormFeedback.js
+++ src/Components/FormFeedback.js
@@ -1,38 +1,53 @@
 import React from 'react';
 
 class FormFeedback extends React.Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  onButtonGood = () => {
+    this.setState(preState => {
+      return {
+        good: preState.good + 1,
+      };
+    });
+  };
+
+  onButtonNeutral = () => {
+    this.setState(preState => {
+      return {
+        neutral: preState.neutral + 1,
+      };
+    });
+  };
+
+  onButtonBad = () => {
+    this.setState(preState => {
+      return {
+        bad: preState.bad + 1,
+      };
+    });
+  };
+
   render() {
     return (
       <div>
         <h1>Please leave feedback</h1>
-        <button
-          type="button"
-          onClick={() => {
-            console.log('КЛикнули на кнопку Good');
-          }}
-        >
+        <button type="button" onClick={this.onButtonGood}>
           Good
         </button>
-        <button
-          type="button"
-          onClick={() => {
-            console.log('КЛикнули на кнопку Neutral');
-          }}
-        >
+        <button type="button" onClick={this.onButtonNeutral}>
           Neutral
         </button>
-        <button
-          type="button"
-          onClick={() => {
-            console.log('КЛикнули на кнопку Bad');
-          }}
-        >
+        <button type="button" onClick={this.onButtonBad}>
           Bad
         </button>
         <h2>Statistics</h2>
-        <span>Good: 3</span>
-        <span>Neutral: 2</span>
-        <span>Bad: 2</span>
+        <span>Good: {this.state.good}</span>
+        <span>Neutral: {this.state.neutral}</span>
+        <span>Bad: {this.state.bad}</span>
       </div>
     );
   }
